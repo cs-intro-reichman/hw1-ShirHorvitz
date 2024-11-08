862121abf3926ae4fd6f8c13670cbce2152f3712
ShirHorvitz <shir.horvitz@post.runi.ac.il>
2024-11-08 21:05:47 +0200

    done

--- Bill3.java
+++ Bill3.java
@@ -8,6 +8,6 @@ public class Bill3 {
 		int totalsum = Integer.parseInt(args[3]);
 		// Calculate how much money each person need to pay
 		double eachprice = Math.ceil(totalsum / 3.0);
-		System.out.println("Dear " + name1 + ", " + name2 + "and " + name3 + ": pay " + eachprice + " shekels each");
+		System.out.println("Dear " + name1 + ", " + name2 + " and " + name3 + ": pay " + eachprice + " shekels each");
 	}
 }
